2099dcc2cec1c88ba977a350b19296298101b749
Tushar Malik <87412356+TusharMalik03@users.noreply.github.com>
2023-05-22 17:35:33 +0530

    Delete src directory

--- src/compo/Alert.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Alert(props) {
-  return(
-      <>
-      {props.warn && <div className="alert ning my-0" role="alert">{props.warn}</div>}
-     </>
-  );
-}
